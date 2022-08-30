8e1e2c808160e2880e1a9c5baa8d54b3a149e8e6
Axel Choteau <axelc37@outlook.fr>
2022-08-30 09:42:50 +0200

    Pages d'accueil
    4 vues :
    - Invitation à s'authentifier
    - Vue famille
    - Vue éducateur
    - Vue admin

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,10 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+    /**
+     * **Page d'accueil** / Tableau de bord
+     *
+     * En fonction de l'utilisateur, afficher :
+     * - Une invitation à s'authentifier
+     * - Le tableau de bord d'une famille
+     * - Le tableau de bord d'un éducateur
+     */
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-        ]);
+        // Choix de la vue en fonction du rôle de l'utilisateur
+
+        $vue = 'accueil/visiteur.html.twig';
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $vue = 'accueil/administrateur.html.twig';
+        } elseif ($this->isGranted('ROLE_EDUCATEUR')) {
+            $vue = 'accueil/educateur.html.twig';
+        } elseif ($this->isGranted('ROLE_FAMILLE')) {
+            $vue = 'accueil/famille.html.twig';
+        }
+
+        return $this->render($vue);
     }
 }
