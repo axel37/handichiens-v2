82955abc04660562ebcc6907dad761730ad6ff12
Axel Choteau <axelc37@outlook.fr>
2022-08-31 10:42:02 +0200

    Contrôleur Disponibilités (lecture seulement)
    - /mes-disponibilites : une famille authentifiée voit ses disponibilités
    - /disponibilite : Un éducateur/admin voit la liste de toutes les disponibilités
    - /disponibilite/{id} : Voir le détail d'une disponibilité
    - /famille/{id}/disponibilite : Un éducateur/admin voit les disponibilités d'une famille spécifique

--- src/Controller/DisponibiliteController.php
+++ src/Controller/DisponibiliteController.php
@@ -2,50 +2,79 @@
 
 namespace App\Controller;
 
+use App\Entity\Disponibilite;
 use App\Entity\Famille;
-use Doctrine\ORM\EntityNotFoundException;
+use App\Repository\DisponibiliteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisponibiliteController extends AbstractController
 {
     /**
-     * - Famille : Disponibilités d'une famille (elle-même)
-     * - Éducateur : Disponibilités de toutes les familles
+     * Planning de la famille authentifiée
      * @return Response
      */
-    #[Route('/disponibilite', name: 'app_disponibilite')]
-    public function index(): Response
+    #[Route('/mes-disponibilites', name: 'app_disponibilite_profil')]
+    public function mesDispo(): Response
     {
-        // NON : "Une route qui affiche deux choses"
-        if ($this->isGranted('ROLE_EDUCATEUR'))
-        {
-            // TODO : Vue : liste de toutes les disponibilités
-            throw new NotFoundHttpException("Cette page n'a pas encore été implémentée ! \n(/disponibilite pour ROLE_EDUCATEUR : Liste des disponibilités de toutes les familles)");
-        }
-        elseif ($this->isGranted('ROLE_FAMILLE') && $this->getUser() instanceof Famille)
-        {
+        if ($this->isGranted('ROLE_FAMILLE') && $this->getUser() instanceof Famille) {
             return $this->renderPlanning($this->getUser());
+        } else {
+            throw new AccessDeniedHttpException("Vous devez être authentifié en tant que famille pour accéder à cette ressource.");
         }
-        else
-        {
-            // TODO : Que faire ici ? S'agit-il d'une Response ou d'une erreur ?
-            //  Il devrait de toute manière être impossible d'arriver ici (security.yaml restreint cette route aux utilisateurs authentifiés)
+    }
+
+    /**
+     * Disponibilités de toutes les familles
+     * @param DisponibiliteRepository $dispoRepo
+     * @return Response
+     */
+    #[Route('/disponibilite', name: 'app_disponibilite_index')]
+    public function index(DisponibiliteRepository $dispoRepo): Response
+    {
+        if ($this->isGranted('ROLE_EDUCATEUR')) {
+
+            $disponibilites = $dispoRepo->findAll();
+            return $this->render('/disponibilite/index.html.twig', [
+                'disponibilites' => $disponibilites
+            ]);
+        } else {
             throw new AccessDeniedHttpException("Vous devez être authentifié pour accéder à cette ressource.");
         }
     }
 
-    public function parFamille(Famille $famille): Response
+    /**
+     * Détails d'une disponibilité (visualisation / modification / suppression ?)
+     * @param Disponibilite $disponibilite
+     * @return Response
+     */
+    #[Route('/disponibilite/{disponibilite}', name: 'app_disponibilite_details')]
+    public function details(Disponibilite $disponibilite): Response
     {
-       return $this->renderPlanning($famille);
+        return $this->render('/disponibilite/details.html.twig', [
+            'disponibilite' => $disponibilite
+        ]);
     }
 
 
     /**
-     * Affiche la liste des disponibilités d'une famille
+     * Disponibilités de la famille en paramètre dans l'url
+     * @param Famille $famille
+     * @return Response
+     */
+    #[Route('famille/{famille}/disponibilite/', name: 'app_disponibilite_famille')]
+    public function parFamille(Famille $famille): Response
+    {
+        return $this->renderPlanning($famille);
+    }
+
+
+    /**
+     * Affiche la liste des disponibilités de la famille passée en paramètre.
+     *
+     * <u>Note :</u>  Ceci n'est pas une route !
      * @param Famille $famille
      * @return Response
      */
