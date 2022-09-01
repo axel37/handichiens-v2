bcc260ec1ff0c68838da485157a4b3cb9f8b815a
Axel <axelc37@outlook.fr>
2022-09-01 23:30:40 +0200

    Amélioration de l'interface
    - Transformation de certains liens en boutons
    - Ajout de titres
    - Retrait d'informations redondantes ou provisoires
    - Utilisation du layout standard dans plus de vues

--- src/Controller/FamilleController.php
+++ src/Controller/FamilleController.php
@@ -6,10 +6,16 @@ use App\Entity\Famille;
 use App\Repository\FamilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FamilleController extends AbstractController
 {
+    /**
+     * Liste de toutes les familles
+     * @param FamilleRepository $familleRepository
+     * @return Response
+     */
     #[Route('/famille', name: 'app_famille_index')]
     public function index(FamilleRepository $familleRepository): Response
     {
@@ -19,7 +25,12 @@ class FamilleController extends AbstractController
         ]);
     }
 
-    #[Route('/famille/{famille}', name: 'app_famille_details')]
+    /**
+     * Détails d'une famille (similaire à la vue "mon-profil"
+     * @param Famille $famille
+     * @return Response
+     */
+    #[Route('/famille/{famille}', name: 'app_famille_details', requirements: ['famille' => '\d+'])]
     public function details(Famille $famille): Response
     {
         return $this->render('utilisateur/details.html.twig', [
@@ -29,4 +40,14 @@ class FamilleController extends AbstractController
 
 //    /famille/{famille}/disponibilite = app_disponibilite_famille dans DisponibiliteController
 
+    /**
+     * Ajout d'une famille (par un éducateur ou un administrateur)
+     * @return Response
+     */
+    #[Route('/famille/ajouter', name: 'app_famille_ajouter')]
+    public function ajouter(): Response
+    {
+        // TODO : Créer (et traiter) le formulaire
+        throw new NotFoundHttpException('Pas encore implémenté !');
+    }
 }
