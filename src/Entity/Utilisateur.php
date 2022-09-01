f02feb4531bb8ee3c8b9adae043e8bd1877de507
Axel <axelc37@outlook.fr>
2022-09-01 17:03:11 +0200

    25 photos (#26)
    * Ajout de vich/uploader-bundle
    
    * Utilisateur/Famille : Photo de profil
    
    * Chiens : Photo de profil
    
    * Retrait d'un TODO :)
    
    * Delete ChienType.php
    
    Fichier de test qui n'aurait pas dû être commit

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -3,19 +3,24 @@
 namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Ignore;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation\Uploadable;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
 #[ORM\InheritanceType('JOINED')]
-#[ORM\DiscriminatorColumn(name: 'type', type:'string')]
+#[ORM\DiscriminatorColumn(name: 'type', type: 'string')]
 #[ORM\DiscriminatorMap(['utilisateur' => Utilisateur::class, 'famille' => Famille::class])]
-class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
+#[Uploadable]
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface, \Serializable
 {
-    // TODO : Photo
-
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -47,6 +52,36 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 25, nullable: true)]
     private ?string $telephone = null;
 
+    /**
+     * Le nom du fichier représentant la photo de profil
+     * @var string|null
+     */
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photoFileName = null;
+
+    /**
+     * Le fichier représentant la photo. N'est pas stocké en base.
+     * @see $photoFileName
+     * @var UploadedFile|File|null
+     */
+    #[Assert\File(
+        maxSize: "1M",
+        mimeTypes: [
+            "image/*"
+        ],
+        mimeTypesMessage: "Le fichier doit être une image."
+    )]
+    #[UploadableField(mapping: 'utilisateur', fileNameProperty: 'photoFileName')]
+    private UploadedFile|File|null $photo = null;
+
+    /**
+     * Requis par Vich/UploaderBundle
+     * @see setPhoto
+     * @var DateTimeImmutable|null
+     */
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $dateModificationPhoto = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -71,7 +106,7 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     /**
@@ -147,5 +182,78 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
         $this->telephone = $telephone;
 
         return $this;
+    }
+
+    public function getPhotoFileName(): ?string
+    {
+        return $this->photoFileName;
+    }
+
+    public function setPhotoFileName(?string $photoFileName): self
+    {
+        $this->photoFileName = $photoFileName;
+
+        return $this;
+    }
+
+    /**
+     * @return UploadedFile|File|null
+     */
+    #[Ignore] // Le Serializer ignorera ce champ
+    public function getPhoto(): UploadedFile|File|null
+    {
+        return $this->photo;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param UploadedFile|File|null $photo
+     */
+    #[Ignore] // Le Serializer ignorera ce champ
+    public function setPhoto(UploadedFile|File|null $photo): self
+    {
+        $this->photo = $photo;
+
+        if (null !== $photo) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->dateModificationPhoto = new DateTimeImmutable();
+        }
+        return $this;
+    }
+
+    private function getDateModification(): ?DateTimeImmutable
+    {
+        return $this->dateModificationPhoto;
+    }
+
+    private function setDateModification(?DateTimeImmutable $dateModification): self
+    {
+        $this->dateModificationPhoto = $dateModification;
+
+        return $this;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->email,
+            $this->password,
+        ]);
+    }
+
+    public function unserialize(string $data)
+    {
+        list(
+            $this->id,
+            $this->email,
+            $this->password,
+            ) = unserialize($data);
     }
 }
