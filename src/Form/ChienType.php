747ebdc8791b9cc7cf0b3cf91bbb34f915d6c00f
Axel <axelc37@outlook.fr>
2022-09-02 10:19:00 +0000

    36 controleur chien (#39)
    * /chien
    
    Vue et ajout des chiens
    
    * Navbar : Les chiens

---
+++ src/Form/ChienType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chien;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class ChienType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $today = new \DateTimeImmutable('today');
+        // 30 ans en arrière
+        $minDate = $today->sub(new \DateInterval('P30Y'));
+
+        $builder
+            ->add('nom')
+            ->add('dateNaissance', DateType::class, [
+                'input' => 'datetime_immutable',
+                'data' => $today,
+                'years' => range($minDate->format('Y'), $today->format('Y')),
+            ])
+            ->add('race')
+            ->add('photo', VichImageType::class, [
+                'required' => false
+            ])
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Chien::class,
+        ]);
+    }
+}
