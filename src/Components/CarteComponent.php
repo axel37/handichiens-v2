79e8f7238277a588cb162277169932bf46372d16
ViolaineR <95360921+ViolaineR@users.noreply.github.com>
2022-09-05 09:47:38 +0200

    22 composant carte (#42)
    * Creation du composant carte
    
    * Fix Création du composant carte
    
    * Ajouter l'affichage des informations des entités
    
    * Ajout de types sur les cartes, exemples disponible sur la vue accueil administrateur
    
    * Composant Carte : Refactoring
    
    Le composant accepte une entité OU un href, une/des icônes et un/des textes
    
    * Retrait de tests
    
    (oups)
    
    Co-authored-by: Axel Choteau <axelc37@outlook.fr>

---
+++ src/Components/CarteComponent.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Components;
+
+use App\Entity\Chien;
+use App\Entity\Famille;
+use App\Entity\Affectation;
+use App\Entity\Utilisateur;
+use App\Entity\Disponibilite;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+
+/**
+ * Composant carte
+ *
+ * Accepte une entité **OU** du texte, une/des icônes et un lien.
+ *
+ * @see mount
+ */
+#[AsTwigComponent('carte')]
+class CarteComponent
+{
+    /**
+     * Tableau représentant le texte à écrire dans le composant.
+     *
+     * Un élément = une ligne.
+     *
+     * Si un string est passé dans mount(), il sera transformé en array à 1 élément.
+     * @var array
+     */
+    #[ExposeInTemplate]
+    private array $texte;
+
+    /**
+     * Cible du lien.
+     * @var string
+     */
+    #[ExposeInTemplate]
+    private string $href;
+
+    /**
+     * Chemin des icônes
+     * @var array
+     */
+    #[ExposeInTemplate]
+    private array $icone;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        // Utilisé pour générer les routes
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * Paramètres acceptés à l'instanciation du composant.
+     *
+     * Si $entite est renseignée, les autres paramètres seront ignorés.
+     *
+     * Si $entite *n'est pas* renseignée, les autres paramètres **doivent** être renseignés.
+     * @param Chien|Famille|Disponibilite|Affectation|Utilisateur|null $entite
+     * @param array|string|null $texte Une ligne (string) ou un élément par ligne (array)
+     * @param string|null $href La destination du lien
+     * @param array|string|null $icone La/les icônes à afficher
+     * @return void
+     */
+    public function mount(
+        null|Chien|Famille|Disponibilite|Affectation|Utilisateur $entite = null,
+        null|array|string                                        $texte = null,
+        ?string                                                  $href = null,
+        ?string                                                  $icone = null,
+    ): void
+    {
+        // Si une entité a été passée, remplacer le texte, le href et l'icône
+        if (isset($entite)) {
+            $type = (new \ReflectionClass($entite))->getShortName();
+
+            // TODO : hrefs
+            // TODO : Icônes
+
+            // Définir le contenu de la carte en fonction du type de l'entité
+            switch ($type):
+                case 'Famille':
+                    $texte = [
+                        $entite->getNom() . ' ' . $entite->getPrenom(),
+                        $entite->getAdresse()
+                    ];
+                    $href = $this->urlGenerator->generate('app_accueil');
+                    $icone = '/';
+                    break;
+                case 'Utilisateur':
+                    $texte = [
+                        $entite->getNom() . ' ' . $entite->getPrenom()
+                    ];
+                    $href = $this->urlGenerator->generate('app_accueil');
+                    $icone = '/';
+                    break;
+                case 'Disponibilite':
+                    $texte = [
+                        $entite->getDebut()->format('d.m.Y H:i:s'),
+                        $entite->getFin()->format('d.m.Y H:i:s')
+                    ];
+                    $href = $this->urlGenerator->generate('app_accueil');
+                    $icone = '/';
+                    break;
+                default:
+                    $texte = ['Entité invalide'];
+                    $href = '/';
+                    $icone = '/';
+            endswitch;
+        }
+
+        if (is_string($texte)) {
+            $texte = [$texte];
+        }
+        if (is_string($icone)) {
+            $icone = [$icone];
+        }
+
+        // Si, à ce point du code, une de ces variables est null, le composant n'a pas été créé correctement.
+        try {
+            $this->texte = $texte;
+            $this->href = $href;
+            $this->icone = $icone;
+        } catch (\TypeError $exception) {
+            throw new \TypeError('Erreur lors de la création du composant CarteComponent. 
+            Avez-vous passé tous les paramètres requis à son instantiation ? 
+            (' . $exception->getMessage() . ')');
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getTexte(): array
+    {
+        return $this->texte;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHref(): string
+    {
+        return $this->href;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIcone(): array
+    {
+        return $this->icone;
+    }
+
+}
